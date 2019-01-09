417202321002cc00608d45ce2e73edd2b1c20f18
magicmarie <natukunda162@gmail.com>
2019-01-09 14:49:49 +0300

    remove logic from the views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
+#give access to the navigation_helper file content
+require 'navigation_helper.rb'
+
 module ApplicationHelper
+  #inject this module into application_helper
+  include NavigationHelper
 end
--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,8 @@ module Collabfield
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # helpers are available for corresponding controllers views only
+    config.action_controller.include_all_helpers = false
   end
 end
