2c08213a3a8254a37639ad91f6fc1206bc922f20
magicmarie <natukunda162@gmail.com>
2019-03-28 17:01:28 +0300

    - add pagination - add hobbies, studies and team memebers templates

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,22 @@
 module PostsHelper
+
+  def create_new_post_partial_path
+    if user_signed_in?
+      'posts/partials/create_new_post/signed_in'
+    else
+      'posts/partials/create_new_post/not_signed_in'
+    end
+  end
+
+  def all_categories_button_partial_path
+    if params[:category].blank?
+      'posts/partials/categories/all_selected'
+    else
+      'posts/partials/categories/all_not_selected'
+    end
+  end
+
+  def no_posts_partial_path
+    @posts.empty? ? 'posts/partials/no_posts' : 'shared/empty_partial'
+  end
 end
