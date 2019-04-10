4742fa7339d197bd4c0e651198e5c0e8acbb34a9
magicmarie <natukunda162@gmail.com>
2019-04-10 11:51:05 +0300

    write tests for the post validations

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :post do
     title {Faker::Name.unique.name}
-    content {Faker::Lorem.sentence}
+    content {Faker::Lorem.sentence(word_count = 21)}
     user
     category
   end
